7e47f8586bec8014e700a9dbfe6cb895ce6b8978
koolgal006 <46749896+koolgal006@users.noreply.github.com>
2022-12-14 19:23:37 +0700

    Update curationreport.js

--- curationreport.js
+++ curationreport.js
@@ -26,7 +26,7 @@ class Methods {
     }
     async main() { 
         let t = this; 
-        let trx = await this.getAccountHistory('https://api.steemit.com', [this.user,-1, 100]);
+        let trx = await this.getAccountHistory('https://api.steemit.com', [this.user,1, 1200000]);
         trx = JSON.parse(trx).result.reverse();
         let votes = this.filterByVotes(trx);
         votes     = this.filterByTime(votes);
@@ -111,7 +111,6 @@ class Methods {
         }else {
             console.log('no votes found');
         }
-       process.exit();
     }
     filterByVotes(trx){
         let votes = []; 
